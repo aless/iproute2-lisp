bd6f0445cca256bb6fd4b13db3e6eaeb435ace2a
Alex Lorca <alex.lorca@gmail.com>
2010-08-10 23:50:39 -0400

    ip: added ip map show

--- ip/ipmap.c
+++ ip/ipmap.c
@@ -6,6 +6,9 @@
 
 #include "utils.h"
 
+#define PREFLEN 19
+#define ADDRLEN 16
+
 struct map {
 	inet_prefix	eid;
 	inet_prefix	rloc;
@@ -34,6 +37,37 @@ static int parse_cb(struct nl_msg *msg, void *arg)
 
 	printf("response type: %d\n", nlh->nlmsg_type);
 	return 0;
+}
+
+static int parse_show(struct nl_msg *msg, void *arg)
+{
+	struct nlmsghdr *nlh = nlmsg_hdr(msg);
+	struct nlattr *attrs[LISP_GNL_ATTR_MAX+1], *att;
+	int err, cnt;
+	char eid[PREFLEN + 4];
+	char saddr[ADDRLEN];
+
+	struct in_addr addr;
+
+	err = genlmsg_parse(nlh, 0, attrs, LISP_GNL_ATTR_MAX, lisp_gnl_policy);
+
+	nla_for_each_nested(att, attrs[LISP_GNL_ATTR_MAP], cnt) {
+		switch(nla_type(att)) {
+		case LISP_GNL_ATTR_MAP_EID:
+			addr.s_addr = nla_get_u32(att);
+			format_host(AF_INET, 0, &addr, saddr, 16);
+			break;
+		case LISP_GNL_ATTR_MAP_EIDLEN:
+			sprintf(eid, "eid %s/%d", saddr, nla_get_u8(att));
+			break;
+		case LISP_GNL_ATTR_MAP_RLOC:
+			addr.s_addr = nla_get_u32(att);
+			printf("%s via rloc %s\n", eid, format_host(AF_INET, 0, &addr, saddr, 16));
+			break;
+		}
+	}
+
+	return NL_OK;
 }
 
 static int add_error_handler(struct sockaddr_nl *nla, struct nlmsgerr *err,
@@ -88,6 +122,34 @@ int map_add_genl(int cmd, struct map *m)
 	return nl_recvmsgs(sock, cb);
 }
 
+int map_show_genl(int cmd)
+{
+	struct nl_handle *sock;
+	struct nl_msg *msg;
+	int family;
+	struct nlattr *at;
+	struct nl_cb *cb;
+
+	cb = nl_cb_alloc(NL_CB_DEFAULT);
+	nl_cb_set(cb, NL_CB_VALID, NL_CB_CUSTOM, parse_show, NULL);
+	nl_cb_err(cb, NL_CB_CUSTOM, add_error_handler, NULL);
+
+	sock = nl_handle_alloc();
+	genl_connect(sock);
+
+	family = genl_ctrl_resolve(sock, LISP_GNL_NAME);
+
+	msg = nlmsg_alloc();
+	genlmsg_put(msg, NL_AUTO_PID, NL_AUTO_SEQ, family, 0,
+		    NLM_F_DUMP, cmd, 1);
+
+	nl_send_auto_complete(sock, msg);
+	nlmsg_free(msg);
+
+	return nl_recvmsgs(sock, cb);
+}
+
+
 static int parse_args(int argc, char **argv, int cmd, struct map *m)
 {
 	int count = 0;
@@ -141,7 +203,7 @@ static int parse_args(int argc, char **argv, int cmd, struct map *m)
 
 static int do_show(int cmd, int argc, char **argv)
 {
-	return 0;
+	return map_show_genl(cmd);
 }
 
 static int do_add(int cmd, int argc, char **argv)
